5510fecd308ad6d147d4674ad90e7f760887a04d
Claire Ramming <claire.ramming@kevala.com>
2025-02-12 21:10:06 -0500

    update allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['human-coralie-tvtea-6783c1aa.koyeb.app']
 
 STATIC_ROOT = 'backend/static'
 
